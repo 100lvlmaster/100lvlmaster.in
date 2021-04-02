36cebcc66266ef1703f47cf76333061a758d7acd
100lvlmaster <naavinkodag@gmail.com>
2021-04-03 02:58:22 +0530

    update

--- pages/work.tsx
+++ pages/work.tsx
@@ -25,67 +25,32 @@ const WorkPage = () => {
           <h2>Side projects</h2>
           <div>
             <ul>
-              <li>
-                <a href={""} target="_blank_">
-                  <div className="pt-5 pb-5">
-                    <img
-                      src={`https://raw.githubusercontent.com/100lvlmaster/maymay/development/assets/icon/logo.png`}
-                      className="rounded w-auto h-20"
-                      alt={`thumbnail-maymay.png`}
-                    />
-                    <div className="text-sm flex flex-row py-4 text-gray-600 dark:text-gray-400">
-                      <div className="flex flex-col">
-                        <span
-                          className={`text-xl dark:text-white text-gray-700 `}
-                        >{`Maymay`}</span>
-                        <span>{`maymay is an open source flutter app made to spawn random memes from reddit and have the user scroll through them as if a social media app but purely for memes.`}</span>
-                      </div>
-                      <div className="px-3 self-start">
-                        <svg
-                          fill="currentColor"
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z" />
-                        </svg>
-                      </div>
-                    </div>
-                  </div>
-                </a>
-              </li>
+              <Card
+                key={`https://raw.githubusercontent.com/100lvlmaster/maymay/development/assets/icon/logo.png`}
+                title={`Maymay v2`}
+                thumbnailSize={"h-28 w-28"}
+                thumbnail={`https://raw.githubusercontent.com/100lvlmaster/maymay/development/assets/icon/logo.png`}
+                description={`maymay is an open source flutter app made to spawn random memes from reddit and have the user scroll through them as if a social media app but purely for memes.`}
+              />{" "}
+              <Card
+                key={`https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_flutter_icon_130599.png`}
+                title={`Stacked page view`}
+                thumbnailSize={"h-28 w-28"}
+                thumbnail={`https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_flutter_icon_130599.png`}
+                description={`A Flutter pacakge that provides vertical pageview carousel in flutter`}
+              />
             </ul>
           </div>
           <h2>Articles</h2>
           {isMounted ? (
             <ul>
-              {articles.data.items.map((item: any, key: number) => (
-                <li key={item.guid + key}>
-                  <a href={item.guid} target="_blank_">
-                    <div className="pt-5 pb-5">
-                      <img
-                        src={item.thumbnail}
-                        className="rounded"
-                        alt={`thumbnail-${item.thumbnail}`}
-                      />
-                      <div className="text-sm flex flex-row py-4 text-gray-600 dark:text-gray-400">
-                        {item.title}
-                        <div className="px-3 self-start">
-                          <svg
-                            fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                          >
-                            <path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z" />
-                          </svg>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                </li>
+              {articles.data.items.map((item: any) => (
+                <Card
+                  key={item.guid}
+                  href={item.guid}
+                  description={item.title}
+                  thumbnail={item.thumbnail}
+                />
               ))}
             </ul>
           ) : (
@@ -101,3 +66,38 @@ const WorkPage = () => {
   );
 };
 export default WorkPage;
+
+const Card = (props) => {
+  return (
+    <li key={props.guid}>
+      <a href={props.href} target="_blank_">
+        <div className="pt-5 pb-5">
+          <img
+            src={props.thumbnail}
+            className={"rounded ".concat(props.thumbnailSize)}
+            alt={`${props.thumbnail}`}
+          />
+          <div className="text-sm flex flex-row py-4 text-gray-600 dark:text-gray-400">
+            <div className={`flex flex-col`}>
+              <span className={`text-black dark:text-white text-lg`}>
+                {props.title}
+              </span>
+              <span>{props.description}</span>
+            </div>
+            <div className="px-3 self-start">
+              <svg
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </a>
+    </li>
+  );
+};
