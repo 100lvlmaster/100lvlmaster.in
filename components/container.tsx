05b493f58427df285b21855ac4fd802e709554fb
100lvlmaster <navinkodag@gmail.com>
2021-04-22 16:43:03 +0530

    update1

--- components/container.tsx
+++ components/container.tsx
@@ -1,12 +1,34 @@
 import { Footer } from "./footer/footer";
 import "./navbar/navbar";
 import { NavBar } from "./navbar/navbar";
+import Head from "next/head";
 export const Container = (props) => {
   return (
     <div
       id={props.id}
       className="dark:bg-black flex flex-col dark:text-white text-black w-screen bg-white items-stretch justify-items-stretch"
     >
+      <Head>
+        <link
+          rel="icon"
+          href="/assets/website_title_link.png"
+          type="image/x-icon"
+        />
+        <title>navinko</title>
+        <html lang={`en`} />
+
+        <meta charSet="UTF-8" />
+        <meta name="description" content="navin's website" />
+        <meta
+          name="keywords"
+          content="HTML, CSS, JavaScript,Next.js,Tailwind"
+        />
+        <meta name="author" content="Navin Kodag" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+      </Head>
       {props.hideNav ?? false ? `` : <NavBar />}
       <div
         className={"flex flex-col justify-items-stretch md:px-40 lg:px-80 text-lg pt-28 p-10"
