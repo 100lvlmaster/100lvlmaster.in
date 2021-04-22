61396886bfae5141d4c1316319d762361347622d
100lvlmaster <navinkodag@gmail.com>
2021-04-22 16:36:31 +0530

    another_update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,27 +7,6 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
-      <Head>
-        <link
-          rel="icon"
-          href="/assets/website_title_link.png"
-          type="image/x-icon"
-        />
-        <title>navinko</title>
-        <html lang={`en`} />
-
-        <meta charSet="UTF-8" />
-        <meta name="description" content="navin's website" />
-        <meta
-          name="keywords"
-          content="HTML, CSS, JavaScript,Next.js,Tailwind"
-        />
-        <meta name="author" content="Navin Kodag" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        ></meta>
-      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
