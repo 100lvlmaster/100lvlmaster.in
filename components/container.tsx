cf54f8bb7745c245753c0acc1eebae9928a082ac
100lvlmaster <navinkodag@gmail.com>
2021-05-08 11:15:44 +0530

    conditional_render_head

--- components/container.tsx
+++ components/container.tsx
@@ -8,27 +8,31 @@ export const Container = (props) => {
       id={props.id}
       className="dark:bg-black flex flex-col dark:text-white text-black w-full bg-white items-stretch justify-items-stretch"
     >
-      <Head>
-        <link
-          rel="icon"
-          href="/assets/website_title_link.png"
-          type="image/x-icon"
-        />
-        <title>navinko</title>
-        <html lang={`en`} />
+      {props.hideNav ?? true ? (
+        ``
+      ) : (
+        <Head>
+          <link
+            rel="icon"
+            href="/assets/website_title_link.png"
+            type="image/x-icon"
+          />
+          <title>navinko</title>
+          <html lang={`en`} />
 
-        <meta charSet="UTF-8" />
-        <meta name="description" content="navin's website" />
-        <meta
-          name="keywords"
-          content="HTML, CSS, JavaScript,Next.js,Tailwind"
-        />
-        <meta name="author" content="Navin Kodag" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        ></meta>
-      </Head>
+          <meta charSet="UTF-8" />
+          <meta name="description" content="navin's website" />
+          <meta
+            name="keywords"
+            content="HTML, CSS, JavaScript,Next.js,Tailwind"
+          />
+          <meta name="author" content="Navin Kodag" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          ></meta>
+        </Head>
+      )}
       {props.hideNav ?? false ? `` : <NavBar />}
       <div
         className={"flex flex-col justify-items-stretch md:px-40 lg:px-80 text-lg md:py-28 py-28 md:p-10 px-5"
