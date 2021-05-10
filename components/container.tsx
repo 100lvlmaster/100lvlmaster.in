a0d86f3cc8e60b61c8024e57cb5e632d546675da
100lvlmaster <navinkodag@gmail.com>
2021-05-11 02:53:18 +0530

    update_meta_tags

--- components/container.tsx
+++ components/container.tsx
@@ -1,17 +1,51 @@
 import { Footer } from "./footer/footer";
 import "./navbar/navbar";
 import { NavBar } from "./navbar/navbar";
+import { useRouter } from "next/router";
 import Head from "next/head";
 export const Container = (props) => {
+  const router = useRouter();
+  const { showFooter, id, hideNav, children, customMeta, className } = props;
+  const meta = {
+    title: "Navin Kodag – Developer",
+    description: `Full stack developer, onw to 10x developer 🚀`,
+    image: "/assets/website_title_link.png",
+    type: "website",
+    ...customMeta,
+  };
   return (
     <div
-      id={props.id}
+      id={id}
       className="dark:bg-black flex flex-col dark:text-white text-black w-full bg-white items-center justify-items-stretch py-5"
     >
-      {props.hideNav ?? true ? (
+      {hideNav ?? true ? (
         ``
       ) : (
         <Head>
+          <title>{meta.title}</title>
+          <meta name="robots" content="follow, index" />
+          <meta content={meta.description} name="description" />
+          <meta
+            property="og:url"
+            content={`https://navinkodag.netlify.app${router.asPath}`}
+          />
+          <link
+            rel="canonical"
+            href={`https://navinkodag.netlify.app${router.asPath}`}
+          />
+          <meta property="og:type" content={meta.type} />
+          <meta property="og:site_name" content="Navin Kodag" />
+          <meta property="og:description" content={meta.description} />
+          <meta property="og:title" content={meta.title} />
+          <meta property="og:image" content={meta.image} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@100lvlmaster" />
+          <meta name="twitter:title" content={meta.title} />
+          <meta name="twitter:description" content={meta.description} />
+          <meta name="twitter:image" content={meta.image} />
+          {meta.date && (
+            <meta property="article:published_time" content={meta.date} />
+          )}
           <link
             rel="icon"
             href="/assets/website_title_link.png"
@@ -33,14 +67,14 @@ export const Container = (props) => {
           ></meta>
         </Head>
       )}
-      {props.hideNav ?? false ? `` : <NavBar />}
+      {hideNav ?? false ? `` : <NavBar />}
       <div
         className={"flex flex-col justify-items-stretch max-w-2xl px-8 py-5 md:px-16"
           .concat(" ")
-          .concat(props.className)}
+          .concat(className)}
       >
-        {props.children}
-        {props.showFooter ?? true ? <Footer className="py-10" /> : ``}
+        {children}
+        {showFooter ?? true ? <Footer className="py-10" /> : ``}
       </div>
     </div>
   );
