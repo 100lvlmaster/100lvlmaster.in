aa3fc1bab3f8e5bf7ca872af6dcf3c2146ad214b
100lvlmaster <navinkodag@gmail.com>
2021-05-25 16:50:52 +0530

    minor_fix

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -10,7 +10,7 @@ const BlogPage = () => {
   //
   const loadArticles = async () => {
     setArticles(await fetchArticles());
-    setMounted(true);
+    // setMounted(true);
   };
 
   //
@@ -21,23 +21,21 @@ const BlogPage = () => {
   }, []);
   return (
     <div>
-      <Container className="flex flex-col">
-        <div className=" flex  flex-col">
-          <h2 className="font-bold text-xl">Articles</h2>
-          <ul className="flex flex-col py-5 items-stretch">
-            {isMounted
-              ? articles.data.items.map((item: any) => (
-                  <ArticleCard
-                    key={item.guid}
-                    href={item.guid}
-                    title={item.title}
-                    description={""}
-                    imgUrl={item.thumbnail}
-                  />
-                ))
-              : [1, 2, 3].map((item) => <PulseLoader key={item} />)}
-          </ul>
-        </div>
+      <Container className="flex flex-col w-screen">
+        <h2 className="font-bold text-xl">Articles</h2>
+        <ul className="flex flex-col py-5 items-stretch ">
+          {false
+            ? articles.data.items.map((item: any) => (
+                <ArticleCard
+                  key={item.guid}
+                  href={item.guid}
+                  title={item.title}
+                  description={""}
+                  imgUrl={item.thumbnail}
+                />
+              ))
+            : [1, 2, 3].map((item) => <PulseLoader key={item} />)}
+        </ul>
       </Container>
     </div>
   );
@@ -52,15 +50,15 @@ const ArticleCard = ({ title, imgUrl, description, href }) => (
     target="_blank_"
   >
     <div className="flex flex-row">
-      <div className="flex-grow flex flex-col p-3 overflow-ellipsis	">
-        <div>{title}</div>
-        <div>{description}</div>
-      </div>
       <img
         className="h-16 w-16 object-cover"
         src={imgUrl}
         alt={`${imgUrl}.png`}
       />
+      <div className="flex-grow flex flex-col p-3 overflow-ellipsis	">
+        <div>{title}</div>
+        <div>{description}</div>
+      </div>
     </div>
   </a>
 );
@@ -69,8 +67,8 @@ const PulseLoader = () => {
   return (
     <div className="border border-gray-200 dark:border-gray-800 m-1 rounded hover:shadow-md text-sm my-1 p-2">
       <div className="flex flex-row space-x-2">
-        <div className={"h-5 w-80".concat(loader)}>{}</div>
-        <div className={"h-16 w-16".concat(loader)}></div>
+        <div className={"h-16 w-16 ".concat(loader)}></div>
+        <div className={"h-5 w-max flex-grow p-3 ".concat(loader)}>{}</div>
       </div>
     </div>
   );
