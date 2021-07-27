080cd3c2bf17b3ae6d5179a7841b78bc47b88e72
Navin Kodag <navinkodag@gmail.com>
2021-07-27 08:29:02 +0530

    update

--- components/layouts/blog.tsx
+++ components/layouts/blog.tsx
@@ -4,27 +4,23 @@ import dateformat from "dateformat";
 const BlogLayout = ({ children, frontMatter }) => {
   frontMatter = frontMatter as FrontMatter;
   return (
-    <Container metaData={frontMatter}>
-      <article className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
-        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
-          {frontMatter.title}
-        </h1>
-        <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full mt-2">
-          <div className="flex items-center">
-            <p className="text-sm text-gray-700 dark:text-gray-300">
-              {"Navin Kodag"}
-              {` • `}
-              {`${frontMatter.readingTime} mins`}
-            </p>
-          </div>
-          <p className="text-sm text-gray-500 min-w-32 mt-2 md:mt-0">
-            {dateformat(frontMatter.publishedAt, `dd mmm yyyy`)}
+    <Container metaData={frontMatter} className={`prose`}>
+      <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
+        {frontMatter.title}
+      </h1>
+      <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full mt-2">
+        <div className="flex items-center">
+          <p className="text-sm text-gray-700 dark:text-gray-300">
+            {"Navin Kodag"}
+            {` • `}
+            {`${frontMatter.readingTime} mins`}
           </p>
         </div>
-        <div className="prose dark:prose-dark max-w-none w-full">
-          {children}
-        </div>
-      </article>
+        <p className="text-sm text-gray-500 min-w-32 mt-2 md:mt-0">
+          {dateformat(frontMatter.publishedAt, `dd mmm yyyy`)}
+        </p>
+      </div>
+      <div className="prose dark:prose-dark w-full">{children}</div>
     </Container>
   );
 };
