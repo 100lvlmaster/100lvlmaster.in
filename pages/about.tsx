f23243f9b57ae28bcb8527732031478484659b36
100lvlmaster <navinkodag@gmail.com>
2021-09-10 02:51:15 +0530

    minor 🧹🧹

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,19 +7,18 @@ const About = () => {
         <h2 className="font-black text-4xl">About</h2>
         <div className="dark:text-gray-400 text-sm text-gray-800 py-10">
           <p>
-            {`
-              I am a full-stack junior developer at alekha.net, 
-              I started out as a self-taught developer and 
-              now I am a third-year student pursuing a degree in Computer Science here in India.
-                `}
+            I started out as a self-taught teenage script kiddie at 16. Then
+            pursued bachelors in Computer Science. Now I&apos;m in the last year
+            of my degree. Having interned at 2 startups and 6 months of
+            freelancing experience.
           </p>
           <br />
           <p>
-            {`I mostly work on Flutter and Next.js on front-end. And use Laravel,
-            Node.js and Firebase for back-end.`}
+            Now I build efficient products using different tech stacks. I use
+            Flutter, React, NextJs or Svelte for the Front-end. With Node.js,
+            NestJs, Gin-gonic, Gofiber or Firebase for Back-end. I&apos;ve
+            created consistent and realtime APIs with GraphQL and REST.
           </p>
-          <br />
-          <p>{`I've got experience using and writing REST APIs and GraphQL`}</p>
         </div>
         <TimelineComponent />
       </Container>
