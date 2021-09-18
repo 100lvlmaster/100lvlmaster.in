5fa8b4399342f4e8a5d2888230ece1f6bee7fa70
100lvlmaster <navinkodag@gmail.com>
2021-09-19 02:58:33 +0530

    fix: use devto api

--- lib/blog.ts
+++ lib/blog.ts
@@ -4,18 +4,16 @@ const requestInit: RequestInit = {
   cache: "no-cache",
   headers: {
     "Content-Type": "application/json",
-    // "api-key": process.env.NEXT_PUBLIC_DEVTO_API_KEY,
+    "api-key": process.env.NEXT_PUBLIC_DEVTO_API_KEY,
   },
 };
 
 export const blogArticles = async (): Promise<Post[]> => {
   const unSortedPosts: Post[] = (await (
-    await fetch(
-      `https://dev.to/api/articles?username=100lvlmaster`,
-      requestInit
-    )
+    await fetch(`https://dev.to/api/articles/me/`, requestInit)
   ).json()) as Post[];
   /// Sort by descending
+
   return unSortedPosts.sort((prev, curr) =>
     prev.published_at > curr.published_at ? -1 : 1
   );
