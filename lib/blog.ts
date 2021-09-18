c7f992b2371525a0681fe6481f8ba2247201d0dd
100lvlmaster <navinkodag@gmail.com>
2021-09-19 03:45:57 +0530

    update

--- lib/blog.ts
+++ lib/blog.ts
@@ -1,29 +1,29 @@
 import { Post } from "./types";
 
 const requestInit: RequestInit = {
-  cache: "no-cache",
+  cache: "only-if-cached",
   headers: {
-    "Content-Type": "application/json",
-    "User-Agent":
-      "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/W.X.Y.Z Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+    "User-Agent": "*",
     "api-key": process.env.NEXT_PUBLIC_DEVTO_API_KEY,
   },
 };
 
 export const blogArticles = async (): Promise<Post[]> => {
-  const unSortedPosts: Post[] = (await (
-    await fetch(`https://dev.to/api/articles/me/`, requestInit)
-  ).json()) as Post[];
+  const response = await fetch(`https://dev.to/api/articles/me/`, requestInit);
+  const unSortedPosts: Post[] = (await response.json()) as Post[];
   /// Sort by descending
-
+  console.log("building");
   return unSortedPosts.sort((prev, curr) =>
     prev.published_at > curr.published_at ? -1 : 1
   );
+  return posts;
 };
 
 export const articleById = async (id: number): Promise<Post> => {
-  const post = (await (
-    await fetch(`https://dev.to/api/articles/${id}`, requestInit)
-  ).json()) as Post;
+  const response = await fetch(
+    `https://dev.to/api/articles/${id}`,
+    requestInit
+  );
+  const post = (await response.json()) as Post;
   return post;
 };
