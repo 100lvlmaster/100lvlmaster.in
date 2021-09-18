9539f879f9e1e8fe75f415fc8512f8bb9d080cb4
100lvlmaster <navinkodag@gmail.com>
2021-09-19 03:19:19 +0530

    update: fix

--- lib/blog.ts
+++ lib/blog.ts
@@ -4,6 +4,8 @@ const requestInit: RequestInit = {
   cache: "no-cache",
   headers: {
     "Content-Type": "application/json",
+    "User-Agent":
+      "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/W.X.Y.Z Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
     "api-key": process.env.NEXT_PUBLIC_DEVTO_API_KEY,
   },
 };
