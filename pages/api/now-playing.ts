b3e3ab31c348892cf789472a7f45b667cda5a7c9
100lvlmaster <navinkodag@gmail.com>
2022-02-27 05:57:11 +0530

    minor

--- pages/api/now-playing.ts
+++ pages/api/now-playing.ts
@@ -5,8 +5,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  return res.status(200).json({ isPlaying: false });
-
   const response = await getNowPlaying();
 
   if (response.status === 204 || response.status > 400) {
