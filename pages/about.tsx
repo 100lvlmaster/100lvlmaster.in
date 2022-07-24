61de4693701766c1ad228eb70f6d1ad0f67d91a0
100lvlmaster <navinkodag@gmail.com>
2022-07-25 01:53:20 +0530

    update: about

--- pages/about.tsx
+++ pages/about.tsx
@@ -10,16 +10,9 @@ const AboutPage: NextPage = () => {
 
       <Box py="30px">
         <Text>
-          I started out as a self-taught teenage script kiddie at 16. Then
-          pursued bachelors in Computer Science. Now I&apos;m in the last year
-          of my degree. I kept doing what I loved which was learning to solve
-          problems with tech.
-        </Text>
-        <br />
-        <Text>
-          Now I build efficient products using different tech stacks. I use
-          Flutter, React, NextJs or Svelte for the Front-end. With Node.js,
-          NestJs, Gin-gonic, Gofiber or Firebase for Back-end. I&apos;ve created
+          I build efficient products using different tech stacks. I use Flutter,
+          React, NextJs or Svelte for the Front-end. With Node.js, NestJs,
+          Gin-gonic, Gofiber or Firebase for Back-end. I&apos;ve created
           consistent and realtime APIs with GraphQL and REST.
         </Text>
       </Box>
