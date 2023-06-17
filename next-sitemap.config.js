3313dd3e27dce01226822e54ba44323ea272351a
100lvlmaster <navinkodag@gmail.com>
2023-06-17 22:57:47 +0530

    update

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://www.100lvlmaster.com",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
